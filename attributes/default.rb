ef21ecd3fac673e065a341b4e77f4a16bc8e268e
karuru6225 <e2hjhi@gmail.com>
2015-09-28 23:07:09 +0900

    jdkも入れるように。

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@ case platform
 			}
 		default['java']['openjdk']['packages'] = [
 				"java-1.8.0-openjdk.x86_64",
+				"java-1.8.0-openjdk-devel.x86_64",
 				"java-1.8.0-openjdk-headless.x86_64"
 			]
 	when "debian", "ubuntu"
